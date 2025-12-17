037170f0dd16354593fe2066be51ca3b450f3bf0
Pokedextra <248019530+Pokedextra@users.noreply.github.com>
2025-12-17 21:16:32 +0000

    feature: update fetching pokemon - updated fetch request url to fetch pokemon-species data

--- app/static/js/pokemon.js
+++ app/static/js/pokemon.js
@@ -1,10 +1,10 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
-const MAX_POKEMON = 151;
+const MAX_POKEMON = 1025;
 const pokedexGrid = document.querySelector(".pokedex-grid");
 
 let allPokemon = [];
 
-fetch(`${BASE_URL}/pokemon?limit=${MAX_POKEMON}`)
+fetch(`${BASE_URL}/pokemon-species?limit=${MAX_POKEMON}`)
 .then((response) => response.json())
 .then((data) => {
     allPokemon = data.results;
