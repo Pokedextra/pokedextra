6c57d527294b794f00f8ed7089e402f0caa99e8d
Pokedextra <248019530+Pokedextra@users.noreply.github.com>
2025-12-17 19:22:51 +0000

    feature: add fetching pokemon - added fetch request to fetch first 151 from pokeapi and output result to console

--- app/static/js/pokemon.js
+++ app/static/js/pokemon.js
@@ -1,1 +1,12 @@
-console.log('pokemon.js loaded successfully')
+const BASE_URL = "https://pokeapi.co/api/v2/";
+const MAX_POKEMON = 151;
+const pokedexGrid = document.querySelector(".pokedex-grid");
+
+let allPokemon = [];
+
+fetch(`${BASE_URL}/pokemon?limit=${MAX_POKEMON}`)
+.then((response) => response.json())
+.then((data) => {
+    allPokemon = data.results;
+    console.log(data);
+})
