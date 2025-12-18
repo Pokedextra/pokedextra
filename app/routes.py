9a8423e15729b8318750203c045fbde191b399e6
Pokedextra <248019530+Pokedextra@users.noreply.github.com>
2025-12-18 04:01:20 +0000

    feature: add pokedex entry page - added pokemon.html, pokedex-entry.js - added css to style pokedex-entry-main - added new route to load pokedex entry page when a pokemon is clicked on

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/pokemon/<pokemon_name>")
+def pokemon(pokemon_name):
+    return render_template("pokemon.html", pokemon_name=pokemon_name)
