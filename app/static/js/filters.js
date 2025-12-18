913f433b246f8009ca269e15e9cc08a0c014a23e
Pokedextra <248019530+Pokedextra@users.noreply.github.com>
2025-12-18 00:23:03 +0000

    feature: update button logic - separated button logic between all generations and specific generations

--- app/static/js/filters.js
+++ app/static/js/filters.js
@@ -1,27 +1,44 @@
-const pokedexFilters = document.querySelector(".pokedex-filters");
 const pokedexFiltersGeneration = document.querySelector(".pokedex-filters-generation")
 
 pokedexFiltersGeneration.innerHTML = `
-    <button id="all-generations">All Generations</button>
-    <button id="gen-1">Gen 1</button>
-    <button id="gen-2">Gen 2</button>
-    <button id="gen-3">Gen 3</button>
-    <button id="gen-4">Gen 4</button>
-    <button id="gen-5">Gen 5</button>
-    <button id="gen-6">Gen 6</button>
-    <button id="gen-7">Gen 7</button>
-    <button id="gen-8">Gen 8</button>
-    <button id="gen-9">Gen 9</button>
+    <div class="generation-all">
+      <button id="all-generations">All Generations</button>
+    </div>
+    <div class="generation-numbered">
+      <button id="gen-1">Gen 1</button>
+      <button id="gen-2">Gen 2</button>
+      <button id="gen-3">Gen 3</button>
+      <button id="gen-4">Gen 4</button>
+      <button id="gen-5">Gen 5</button>
+      <button id="gen-6">Gen 6</button>
+      <button id="gen-7">Gen 7</button>
+      <button id="gen-8">Gen 8</button>
+      <button id="gen-9">Gen 9</button>
+    </div>
 `
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const generationButtons = document.querySelectorAll(".pokedex-filters-generation button");
+document.addEventListener("DOMContentLoaded", () => {
+    const allGenerationsButton = document.querySelector(".generation-all button");
+    const generationButtons = document.querySelectorAll(".generation-numbered button");
 
-    generationButtons[0].classList.add('active');
+    // Default: All Generations active
+    allGenerationsButton.classList.add('active');
 
-    generationButtons.forEach((button, index) => {
+    // Click: All Generations
+    allGenerationsButton.addEventListener('click', () =>{
+        // Clear active from other buttons
+        generationButtons.forEach(button => button.classList.remove('active'));
+        // Make button active
+        allGenerationsButton.classList.add('active');
+    });
+
+    // CLick: Numbered generations
+    generationButtons.forEach((button) => {
         button.addEventListener("click", function () {
+            // Clear active from other buttons
             generationButtons.forEach(btn => btn.classList.remove('active'));
+            allGenerationsButton.classList.remove('active');
+            // Make button active
             this.classList.add('active');
         });
     });
